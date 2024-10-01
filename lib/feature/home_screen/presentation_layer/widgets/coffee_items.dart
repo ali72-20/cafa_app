18be0fd2b16f1ab13541d320bfede986efdbe052
ali72-20 <as20201118@gmail.com>
2024-10-01 18:05:23 +0300

    add coffee item

--- lib/feature/home_screen/presentation_layer/widgets/coffee_items.dart
+++ lib/feature/home_screen/presentation_layer/widgets/coffee_items.dart
@@ -1,5 +1,7 @@
+import 'package:cafa_app/core/Style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class CoffeeItems extends StatelessWidget {
   CoffeeItems({super.key});
@@ -19,23 +21,70 @@ class CoffeeItems extends StatelessWidget {
       shrinkWrap: true,
       childAspectRatio: (150 / 195),
       children: [
-        for(int i = 0; i < img.length; ++i)
+        for (int i = 0; i < img.length; ++i)
           Container(
             padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            margin:  const EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 13),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color(0xff212325),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.4),
-                  spreadRadius: 1,
-                  blurRadius: 8
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xff212325),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      spreadRadius: 1,
+                      blurRadius: 8)
+                ]),
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    child: Image.asset(
+                      "lib/core/utilites/Images/img${i + 1}.png",
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(img[i], style: Style.style18.copyWith(fontWeight: FontWeight.bold, color: Colors.white),),
+                        const SizedBox(height: 8,),
+                        const Text("Best Coffee", style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white60
+                        ),)
+                      ],
+                    ),
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Row(
+                    mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("\$30", style: Style.style18.copyWith(color: Colors.white),),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration:  BoxDecoration(
+                          color: const Color(0xffE57734),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Icon(CupertinoIcons.add,size: 20,color: Colors.white,),
+                      )
+                    ],
+                  ),
                 )
-              ]
+              ],
             ),
           ),
-
       ],
     );
   }
